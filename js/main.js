97b54bd1ae1226dce9fe852a8058da9fd02e9b98
ANA1512 <65736679+ANA1512@users.noreply.github.com>
2021-01-18 00:21:12 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 /*********CONNECTION API**************/
 
+    
+ 
     const apiUrl = fetch('http://localhost:3000/api/cameras/')
-         
+    
+        
     apiUrl.then(async(responseData)=>{
         console.log(responseData);
 
         const response = await responseData.json();
         console.log(response);
-        
-        
+              
         try{
 
         const cameraImg= response[0].imageUrl;
         console.log(cameraImg);
-
 
         }catch(err){
           
@@ -21,61 +22,69 @@
 
         }
 
- /* Home Page build*/
+   
 
+
+ /*Home Page build*/
+    
+    //create HTML tag
 	response.forEach(function(responses){ 
     let liste = document.getElementById("test");
 	let imgProd = document.createElement("img");
 	let nameProd = document.createElement("h2");
-	let detailProd = document.createElement("a");
+	let infoProd = document.createElement("a");
 	let priceProd = document.createElement("p");
 	
-	
-
-
-
+     //create attribute
 	imgProd.setAttribute("src",responses.imageUrl);
 	//redirection to Page Product
-	detailProd.setAttribute("href", "product_page.html?id=" + responses._id);
+	infoProd.setAttribute("href", "product_page.html?id=" + responses._id);
 
-	
+	//tag contains
 	nameProd.innerHTML= responses.name;
-	detailProd.innerHTML="En Savoir Plus";
+	infoProd.innerHTML="En Savoir Plus";
 	priceProd.innerHTML= responses.price +" "+"€";
 	
-
-
+    // DOM modif
 	liste.appendChild(imgProd);
 	liste.appendChild(nameProd);
 	liste.appendChild(priceProd);
-	liste.appendChild(detailProd);
-
-  
+	liste.appendChild(infoProd);
 	
   })
  
  });
 
-/*************PRODUCT PAGE****************/
 
+/*************PRODUCT PAGE***************/
+  
+ function oneProduct(){
 
+   // get just Id product from URL
+   
+   let idProduct= location.search.substring(4); // just the number
+   let selectProd= apiUrl;
+   console.log(idProduct +""+selectProd);
+ 
 
-
-	let product= document.getElementById("product_select");
-  	let prodImg =document.createElement("img");
-    let prodName =document.createElement("h2");
-    let prodDescription= document.createElement("p");
-    let prodPrice= document.createElement("p");
-
-
-
-
-
-  //imgProd.setAttribute("src",responses.imageUrl);
+   let product = document.getElementById("product_select");
+   let prodImg =document.createElement("img");
+   let prodName =document.createElement("h2");
+   let prodDescription = document.createElement("p");
+   let prodPrice = document.createElement("p");
 
    product.appendChild(prodImg);
    product.appendChild(prodName);
    product.appendChild(prodPrice);
 
 
-  
+ }  
+
+oneProduct();
+
+
+
+
+	
+
+
