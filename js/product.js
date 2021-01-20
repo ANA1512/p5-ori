c9fd0c91878de398d50a8e8578f98e89f0e60344
ANA1512 <65736679+ANA1512@users.noreply.github.com>
2021-01-20 20:30:36 +0100

    Add files via upload

--- js/product.js
+++ js/product.js
@@ -3,7 +3,7 @@
 
   idCameras:"";
   idCameras= location.search.substring(4); // just the number
-  console.log( idCameras);
+  console.log(idCameras);
  
 
 
@@ -22,31 +22,75 @@ const apiUrl = fetch('http://localhost:3000/api/cameras/')
         //Id page and id for each product
         if(idCameras==response[i]._id){
         
-        //Page structure       
-        let paraName= document.createElement("p");
+        //Page one product structure       
+        let paraName= document.createElement("h3");
+        let imgProd = document.createElement("img");
+        let priceProd = document.createElement("p");
+        let descriptionProd = document.createElement("p");
+        
+        //add img
+        imgProd.setAttribute("src",response[i].imageUrl);
+        
+        //add contains
         paraName.innerHTML=response[i].name;
+        descriptionProd.innerHTML=response[i].description;
+        priceProd.innerHTML= response[i].price +" "+"€";
+        
+        //add to HTML 
         product_select.appendChild(paraName);
+        product_select.appendChild(imgProd);
+        product_select.appendChild(priceProd);
+        product_select.appendChild(descriptionProd);
 
-        //Structure lenses option
+ /*Structure lenses option for HTML page*/
         
-        //let selecCustom = document.createElement("select");
-         let selecCustom = document.createElement("div");
-        let selectlenses = document.createElement("option");
+        let selectCustom = document.getElementById("option_lenses");
         
+        //Create tag for HTML page
+        let customPart = document.createElement("select");
+        let optionChoice = document.createElement("option");
+        let optionOne = document.createElement("option");
+        let optionSecond = document.createElement("option");
 
-        let option =document.getElementById("option_lenses");
-        option.innerHTML=selectlenses;
+        //add tag in HTML page
+        selectCustom.appendChild(customPart);
+        customPart.appendChild(optionChoice);
+        customPart.appendChild(optionOne);
+        customPart.appendChild(optionSecond);
+        
+        //Option choice
+        optionChoice.innerHTML= "Sélectionner une option";
+        optionOne.innerHTML =response[i].lenses[0];
+        optionSecond.innerHTML =response[i].lenses[1];
 
+        /*Redirect button to basket page*/
+
+        let btn = document.getElementById("btn");
+        btn.innerHTML="Ajouter au panier";
+        
+        btn.addEventListener('click', addbasket);
+
+        function addbasket(){
+
+        document.location.href ="basket_page.html?id="+ response[i]._id +"?name=" + response[i].name +"?price="+response[i].price;
+       
+        }
+          
         // Console message
         console.log("you are on this product:"+ " " + response[i].name);
 
-      }else{
+          }else{
 
-        console.log ('Product page not laoded');
-      }
-     }
+             console.log ('Other products page not laoded');
+          }
+
+     };
      
 });
+
+        
+ 
+
 
 
 
