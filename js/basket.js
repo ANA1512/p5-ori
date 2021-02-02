b78aab8d50a235e61c3559d920f586b5f9282dcc
ANA1512 <65736679+ANA1512@users.noreply.github.com>
2021-02-02 11:01:46 +0100

    Add files via upload

--- js/basket.js
+++ js/basket.js
@@ -1,61 +1,39 @@
-idCameras:"";
-  idCameras= location.search.substring(4); // just the number
-  console.log(idCameras);
- 
-/******API FECTH****/
-const apiUrl = fetch('http://localhost:3000/api/cameras/')
+
+let totalAllProd = document.getElementById("total");
+let total =0;
+let buttonSup = document.getElementById("supprimer");
+//let quantity= document.getElementById("quantite").value;
+
+
+async function display(){ 
+
+         let localBasket= await JSON.parse(localStorage.getItem('data'));
+         console.log(localBasket);
+         let itemsdisplay= document.getElementById("items");
         
-    apiUrl.then(async(responseData)=>{
-        console.log(responseData);
+         itemsdisplay.innerHTML =''
 
-        const response = await responseData.json();
-        console.log(response);
- 
-   
-   }); 
+         localBasket.forEach((items,index)=>{ 
+          itemsdisplay.innerHTML += `  
 
-     function display(){ 
+          <table class="table">
+                    <tbody>      
+                        <tr>
+                            <td>${localBasket[index].name}</td>
+                            <td>${localBasket[index].imageUrl}</td>
+                            <td>${localBasket[index].price}</td>
+                            <td><input  type="number" value="1" id="quantite" ></td>
+                            <td><button id="supprimer" onclick= "supprimer('${localBasket[index].name}')">SUPPRIMER</button></td>
+                         </tr>
+                    </tbody>
+                </table>
+           `
 
-        //position dans HTML ok
-        let dispOutput= document.getElementById('test'); 
-        
-        //number of data in LocalStorage
-        console.log(JSON.parse(localStorage.getItem('data')));
-        
-        //loops to get every item on localStorage to html
-        for(let i=0; i<localStorage.getItem('data').length; i++){ 
+           totalAllProd.innerHTML ="total:" +" "+(total+=localBasket[index].price)+ "€"+" "+"("+localBasket.length+")";
+          
 
-        let localBasket= JSON.parse(localStorage.getItem('data'));
-        
-         //HTML tag 
-         let name= document.createElement('p');
-         let price = document.createElement('p');
-         let imgProd = document.createElement('img');
-         let quantity= document.createElement('input');
-
-         //add attribute
-         imgProd.setAttribute("src", localBasket[i].imageUrl);
-         quantity.setAttribute("type", "number");
-         quantity.setAttribute("value", 1);
-         quantity.setAttribute("id","quantity");
-
-         //add tag
-         dispOutput.appendChild(name);
-         dispOutput.appendChild(price);
-         dispOutput.appendChild(quantity);
-
-
-         //tag content
-         name.innerHTML=localBasket[i].name;
-         price.innerHTML=localBasket[i].price;         
-  
-     } 
-  
-  }; 
+       });
+};
 
 display();
-
-
-
-  
 
